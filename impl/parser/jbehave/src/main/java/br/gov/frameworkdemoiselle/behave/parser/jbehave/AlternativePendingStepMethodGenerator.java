005e0ba9bd4b71c33c9ffe5b973ebbdac41d41ad
hudson <mestihudson@gmail.com>
2014-09-27 05:23:49 -0300

    retirada das classes *PendingStepMethodGenerator

--- impl/parser/jbehave/src/main/java/br/gov/frameworkdemoiselle/behave/parser/jbehave/AlternativePendingStepMethodGenerator.java
+++
@@ -1,8 +0,0 @@
-package br.gov.frameworkdemoiselle.behave.parser.jbehave;
-
-import org.jbehave.core.configuration.Keywords;
-import org.jbehave.core.steps.StepCreator.PendingStep;
-
-public interface AlternativePendingStepMethodGenerator {
-	public String execute(Keywords keywords, PendingStep step);
-}
